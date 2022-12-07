92445a0ce8772068ede689a4932227dd1144cb57
Pepe <pepe.eulzer@uni-jena.de>
2022-12-07 15:35:52 +0100

    export metadata

--- scripts/export_lumen_centerlines.py
+++ scripts/export_lumen_centerlines.py
@@ -1,16 +1,20 @@
 import os
 import shutil
+import json
 import vtk
 from glob import glob
 
 """
 Script to export lumen .stl and centerline .obj files for all patients that have both
 for use in CFD modelling.
+Also exports a .csv with meta information on each stenosis.
 Requires VTK >= 8.2 (obj writer).
 """
 
 src_folder = 'C:/Users/Pepe Eulzer/Nextcloud/carotis_data'
 dst_folder = 'C:/Users/Pepe Eulzer/Desktop/daten_kevin'
+
+metadata = [["ID", "Stenosis Degree", "Min Diameter Position", "Min Diameter Normal", "Ref Diameter Position", "Ref Diameter Normal"]]
 
 # iterate patients
 for patient_src_folder in glob(os.path.join(src_folder, "patient*")):
@@ -40,5 +44,31 @@ for patient_src_folder in glob(os.path.join(src_folder, "patient*")):
             writer.SetInputData(reader.GetOutput())
             writer.SetFileName(centerlines_dst_filepath)
             writer.Write()
+
+            # add to metadata
+            meta_filepath = os.path.join(patient_src_folder, "models", (patient_id + "_" + side + "_meta.txt"))
+            meta_entry = [patient_id + "_" + side, [], [], [], [], []]
+            if os.path.exists(meta_filepath):
+                try:
+                    with open(meta_filepath, 'r') as f:
+                        d = json.load(f)
+                        meta_entry = [patient_id + "_" + side,
+                              d["stenosis_degree"],
+                              d["stenosis_min_p"],
+                              d["stenosis_min_n"],
+                              d["poststenotic_p"],
+                              d["poststenotic_n"]]
+                except:
+                    print("WARNING! Classifier module could not load " + meta_filepath)
+            metadata.append(meta_entry)
+
+# save metadata csv
+csv_path = os.path.join(dst_folder, "stenosis_metadata.csv")
+with open(csv_path, 'w') as f:
+    for line in metadata:
+        for item in line:
+            f.write(str(item))
+            f.write(';')
+        f.write('\n')
 
 print("Done!")
